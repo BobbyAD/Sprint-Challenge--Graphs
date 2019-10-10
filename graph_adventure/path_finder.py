2ac9058b6a77710cd141ac4cb4b25dc64541b870
Robert Driskell <driskell.ba@gmail.com>
2019-10-10 17:24:51 -0500

    working on getting it to progress through unexplored rooms

--- graph_adventure/path_finder.py
+++ graph_adventure/path_finder.py
@@ -91,8 +91,16 @@ def path_finder(world):
                 connections.append(current_room.e_to)
             if current_room.w_to:
                 connections.append(current_room.w_to)
+            unexplored_connections = False
             for c in connections:
-                pass
+                if c not in visited:
+                    unexplored_connections = True
+                else:
+                    connections.remove(c)
+            while unexplored_connections == True:
+                # TODO: Weight this to pick a random direction
+                current_room = connections[0]
+
 
     print("**********\nReturn Path")
     for i in return_path:
@@ -148,38 +156,3 @@ def bfs(source_room, destination_room):
                 new_path.append(r)
                 q.insert(0, new_path)
 
-
-
-
-
-
-    """
-    OLD CODE
-    # How many rooms I have to touch
-    shortest_path = []
-
-    # Keeping track of important stuff
-    visited = set()
-    main_queue = []
-    intersections = []
-
-    # Setup
-    main_queue.append(world.startingRoom)
-    current_room = world.startingRoom
-    # while len(visited) < len(world.rooms):
-    if True:
-        current_room = main_queue.pop()
-        visited.add(current_room.id)
-        new_queue = []
-        if current_room.n_to:
-            new_queue.append(current_room.n_to)
-        if current_room.s_to:
-            new_queue.append(current_room.s_to)
-        if current_room.e_to:
-            new_queue.append(current_room.e_to)
-        if current_room.w_to:
-            new_queue.append(current_room.w_to)
-        main_queue += new_queue
-    print(visited)
-    print(main_queue)
-    """
